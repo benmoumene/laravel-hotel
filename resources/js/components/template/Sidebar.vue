61d4ab7441b5d73124906bed385b2cdc5892f698
damianS7 <damian7mg@gmail.com>
2020-01-09 23:25:57 +0100

    Ahora se muestra el avatar y el nombre completo en el sidebar

--- resources/js/components/template/Sidebar.vue
+++ resources/js/components/template/Sidebar.vue
@@ -1,224 +1,237 @@
 <template>
-  <div>
-    <!-- Brand Logo -->
-    <a href="#" class="brand-link">
-      <span class="brand-text font-weight-light">Laravel Hotel</span>
-    </a>
+  <b-container fluid>
+    <b-row>
+      <b-col class="brand-link">
+        <!-- Brand Logo -->
+        <a href="#">
+          <span class="brand-text font-weight-light">Laravel Hotel</span>
+        </a>
+      </b-col>
+    </b-row>
+
+    <b-row class="user-panel">
+      <b-col cols="4" class="pull-left image">
+        <img :src="avatarPath" class="img-circle" alt="User Image" />
+      </b-col>
+      <b-col cols="8" class="pull-left info">
+        <p>
+          <router-link
+            to="/profile"
+            class="nav-link"
+          >{{ appUser.first_name + ' ' + appUser.last_name }}</router-link>
+        </p>
+      </b-col>
+    </b-row>
 
     <!-- Sidebar -->
-    <div class="sidebar">
-      <!-- Sidebar user panel (optional) -->
-      <div class="user-panel mt-3 mb-3 d-flex">
-        <div class="info">
-          <router-link to="/profile" class="nav-link">Welcome, {{ appUser.name }}</router-link>
-        </div>
-      </div>
-
-      <!-- Sidebar Menu -->
-      <nav class="mt-2">
-        <ul
-          class="nav nav-pills nav-sidebar flex-column"
-          data-widget="treeview"
-          role="menu"
-          data-accordion="false"
-        >
-          <li class="nav-item">
-            <router-link to="/" class="nav-link">
-              <i class="nav-icon fa fa-tachometer-alt"></i>
-              Dashboard
-            </router-link>
-          </li>
-          <li class="nav-item has-treeview">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fa fa-users"></i>
-              <p>
-                Customers
-                <i class="fa fa-angle-left right"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <router-link to="/customers/add" class="nav-link">
-                  <i class="nav-icon fa fa-user-plus"></i>
-                  New Customer
-                </router-link>
-              </li>
-              <li class="nav-item">
-                <router-link to="/customers/list" class="nav-link">
-                  <i class="nav-icon fa fa-user-plus"></i>
+    <b-row class="sidebar">
+      <b-col>
+        <!-- Sidebar Menu -->
+        <nav class="mt-2">
+          <ul
+            class="nav nav-pills nav-sidebar flex-column"
+            data-widget="treeview"
+            role="menu"
+            data-accordion="false"
+          >
+            <li class="nav-item">
+              <router-link to="/" class="nav-link">
+                <i class="nav-icon fa fa-tachometer-alt"></i>
+                Dashboard
+              </router-link>
+            </li>
+            <li class="nav-item has-treeview">
+              <a href="#" class="nav-link">
+                <i class="nav-icon fa fa-users"></i>
+                <p>
                   Customers
-                </router-link>
-              </li>
-            </ul>
-          </li>
+                  <i class="fa fa-angle-left right"></i>
+                </p>
+              </a>
+              <ul class="nav nav-treeview">
+                <li class="nav-item">
+                  <router-link to="/customers/add" class="nav-link">
+                    <i class="nav-icon fa fa-user-plus"></i>
+                    New Customer
+                  </router-link>
+                </li>
+                <li class="nav-item">
+                  <router-link to="/customers/list" class="nav-link">
+                    <i class="nav-icon fa fa-user-plus"></i>
+                    Customers
+                  </router-link>
+                </li>
+              </ul>
+            </li>
 
-          <li class="nav-item has-treeview" v-if="isAdmin || isManager">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fa fa-bed"></i>
-              <p>
-                Rooms
-                <i class="fa fa-angle-left right"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <router-link to="/rooms/list" class="nav-link">
-                  <i class="nav-icon fa fa-bed"></i>
-                  Manage Rooms
-                </router-link>
-              </li>
-              <li class="nav-item">
-                <router-link to="/rooms/add" class="nav-link">
-                  <i class="nav-icon fa fa-bed"></i>
-                  Add Room
-                </router-link>
-              </li>
-              <li class="nav-item">
-                <router-link to="/guest/list" class="nav-link">
-                  <i class="nav-icon fa fa-edit"></i>
-                  Guests
-                </router-link>
-              </li>
-              <li class="nav-item">
-                <router-link to="/rooms/availability" class="nav-link">
-                  <i class="nav-icon fa fa-bed"></i>
-                  Availability
-                  <span class="badge badge-info right">6</span>
-                </router-link>
-              </li>
-            </ul>
-          </li>
+            <li class="nav-item has-treeview" v-if="isAdmin || isManager">
+              <a href="#" class="nav-link">
+                <i class="nav-icon fa fa-bed"></i>
+                <p>
+                  Rooms
+                  <i class="fa fa-angle-left right"></i>
+                </p>
+              </a>
+              <ul class="nav nav-treeview">
+                <li class="nav-item">
+                  <router-link to="/rooms/list" class="nav-link">
+                    <i class="nav-icon fa fa-bed"></i>
+                    Manage Rooms
+                  </router-link>
+                </li>
+                <li class="nav-item">
+                  <router-link to="/rooms/add" class="nav-link">
+                    <i class="nav-icon fa fa-bed"></i>
+                    Add Room
+                  </router-link>
+                </li>
+                <li class="nav-item">
+                  <router-link to="/guest/list" class="nav-link">
+                    <i class="nav-icon fa fa-edit"></i>
+                    Guests
+                  </router-link>
+                </li>
+                <li class="nav-item">
+                  <router-link to="/rooms/availability" class="nav-link">
+                    <i class="nav-icon fa fa-bed"></i>
+                    Availability
+                    <span class="badge badge-info right">6</span>
+                  </router-link>
+                </li>
+              </ul>
+            </li>
 
-          <li class="nav-item has-treeview" v-if="isRecepcionist || isAdmin">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fa fa-bed"></i>
-              <p>
-                Reservations
-                <i class="fa fa-angle-left right"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <router-link to="/reservations" class="nav-link">
-                  <i class="nav-icon fa fa-bed"></i>
-                  Manage Reservations
-                </router-link>
-              </li>
-              <li class="nav-item">
-                <router-link to="/reservations" class="nav-link">
-                  <i class="nav-icon fa fa-bed"></i>
-                  New Reservation
-                </router-link>
-              </li>
-            </ul>
-          </li>
+            <li class="nav-item has-treeview" v-if="isRecepcionist || isAdmin">
+              <a href="#" class="nav-link">
+                <i class="nav-icon fa fa-bed"></i>
+                <p>
+                  Reservations
+                  <i class="fa fa-angle-left right"></i>
+                </p>
+              </a>
+              <ul class="nav nav-treeview">
+                <li class="nav-item">
+                  <router-link to="/reservations" class="nav-link">
+                    <i class="nav-icon fa fa-bed"></i>
+                    Manage Reservations
+                  </router-link>
+                </li>
+                <li class="nav-item">
+                  <router-link to="/reservations" class="nav-link">
+                    <i class="nav-icon fa fa-bed"></i>
+                    New Reservation
+                  </router-link>
+                </li>
+              </ul>
+            </li>
 
-          <li class="nav-item has-treeview">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fa fa-hand-holding-usd"></i>
-              <p>
-                Manage Services
-                <i class="fa fa-angle-left right"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <router-link to="/services/add" class="nav-link">
-                  <i class="nav-icon fa fa-hand-holding-usd"></i>
-                  Add Service
-                </router-link>
-              </li>
-              <li class="nav-item">
-                <router-link to="/services/list" class="nav-link">
-                  <i class="nav-icon fa fa-hand-holding-usd"></i>
-                  Show Services
-                </router-link>
-              </li>
-            </ul>
-          </li>
+            <li class="nav-item has-treeview">
+              <a href="#" class="nav-link">
+                <i class="nav-icon fa fa-hand-holding-usd"></i>
+                <p>
+                  Manage Services
+                  <i class="fa fa-angle-left right"></i>
+                </p>
+              </a>
+              <ul class="nav nav-treeview">
+                <li class="nav-item">
+                  <router-link to="/services/add" class="nav-link">
+                    <i class="nav-icon fa fa-hand-holding-usd"></i>
+                    Add Service
+                  </router-link>
+                </li>
+                <li class="nav-item">
+                  <router-link to="/services/list" class="nav-link">
+                    <i class="nav-icon fa fa-hand-holding-usd"></i>
+                    Show Services
+                  </router-link>
+                </li>
+              </ul>
+            </li>
 
-          <li class="nav-item has-treeview">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fa fa-shopping-cart"></i>
-              <p>
-                Inventory
-                <i class="fa fa-angle-left right"></i>
-                <span class="badge badge-danger right">Low Stock!</span>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <router-link to="/inventory" class="nav-link">
-                  <i class="nav-icon fa fa-shopping-cart"></i>
-                  Stock
-                </router-link>
-              </li>
-              <li class="nav-item">
-                <router-link to="/inventory" class="nav-link">
-                  <i class="nav-icon fa fa-shopping-cart"></i>
+            <li class="nav-item has-treeview">
+              <a href="#" class="nav-link">
+                <i class="nav-icon fa fa-shopping-cart"></i>
+                <p>
                   Inventory
-                </router-link>
-              </li>
-            </ul>
-          </li>
+                  <i class="fa fa-angle-left right"></i>
+                  <span class="badge badge-danger right">Low Stock!</span>
+                </p>
+              </a>
+              <ul class="nav nav-treeview">
+                <li class="nav-item">
+                  <router-link to="/inventory" class="nav-link">
+                    <i class="nav-icon fa fa-shopping-cart"></i>
+                    Stock
+                  </router-link>
+                </li>
+                <li class="nav-item">
+                  <router-link to="/inventory" class="nav-link">
+                    <i class="nav-icon fa fa-shopping-cart"></i>
+                    Inventory
+                  </router-link>
+                </li>
+              </ul>
+            </li>
 
-          <li class="nav-item has-treeview" v-if="isManager || isAdmin">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fa fa-wallet"></i>
-              <p>
-                Manage Payments
-                <i class="fa fa-angle-left right"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <router-link to="/billing" class="nav-link">
-                  <i class="nav-icon fa fa-file-invoice-dollar"></i>
-                  Billing
-                  <span class="right badge badge-danger">New</span>
-                </router-link>
-              </li>
-              <li class="nav-item">
-                <router-link to="/billing" class="nav-link">
-                  <i class="nav-icon fa fa-file-invoice-dollar"></i>
-                  Pending Invoices
-                  <span class="right badge badge-danger">6</span>
-                </router-link>
-              </li>
-            </ul>
-          </li>
+            <li class="nav-item has-treeview" v-if="isManager || isAdmin">
+              <a href="#" class="nav-link">
+                <i class="nav-icon fa fa-wallet"></i>
+                <p>
+                  Manage Payments
+                  <i class="fa fa-angle-left right"></i>
+                </p>
+              </a>
+              <ul class="nav nav-treeview">
+                <li class="nav-item">
+                  <router-link to="/billing" class="nav-link">
+                    <i class="nav-icon fa fa-file-invoice-dollar"></i>
+                    Billing
+                    <span class="right badge badge-danger">New</span>
+                  </router-link>
+                </li>
+                <li class="nav-item">
+                  <router-link to="/billing" class="nav-link">
+                    <i class="nav-icon fa fa-file-invoice-dollar"></i>
+                    Pending Invoices
+                    <span class="right badge badge-danger">6</span>
+                  </router-link>
+                </li>
+              </ul>
+            </li>
 
-          <li class="nav-item" v-if="isAdmin">
-            <router-link to="/settings" class="nav-link">
-              <i class="nav-icon fa fa-wrench"></i>
-              Settings
-            </router-link>
-          </li>
-          <li class="nav-item">
-            <router-link to="/about" class="nav-link">
-              <i class="nav-icon fa fa-info"></i>
-              About us
-            </router-link>
-          </li>
-          <li class="nav-item">
-            <router-link to="/profile" class="nav-link">
-              <i class="nav-icon fa fa-user"></i>
-              Profile
-            </router-link>
-          </li>
+            <li class="nav-item" v-if="isAdmin">
+              <router-link to="/settings" class="nav-link">
+                <i class="nav-icon fa fa-wrench"></i>
+                Settings
+              </router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/about" class="nav-link">
+                <i class="nav-icon fa fa-info"></i>
+                About us
+              </router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/profile" class="nav-link">
+                <i class="nav-icon fa fa-user"></i>
+                Profile
+              </router-link>
+            </li>
 
-          <li class="nav-item">
-            <a href="#" @click.prevent="logout()" class="nav-link">
-              <i class="nav-icon fa fa-sign-out-alt"></i>
-              Logout
-            </a>
-          </li>
-        </ul>
-      </nav>
-      <!-- /.sidebar-menu -->
-    </div>
+            <li class="nav-item">
+              <a href="#" @click.prevent="logout()" class="nav-link">
+                <i class="nav-icon fa fa-sign-out-alt"></i>
+                Logout
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <!-- /.sidebar-menu -->
+      </b-col>
+    </b-row>
     <!-- /.sidebar -->
-  </div>
+  </b-container>
 </template>
 <script>
 import { mapGetters, mapState, mapActions } from "vuex";
@@ -231,7 +244,10 @@ export default {
   },
   computed: {
     ...mapGetters(["isAdmin", "isRecepcionist", "isManager"]),
-    ...mapState(["appUser"])
+    ...mapState(["appUser"]),
+    avatarPath() {
+      return "/storage/" + this.appUser.avatar_filename;
+    }
   },
   components: {},
   mounted() {}
@@ -240,5 +256,9 @@ export default {
 <style scoped>
 .nav-treeview li {
   padding-left: 20px;
+}
+.user-panel img {
+  padding: 0.4rem;
+  width: 100%;
 }
 </style>
