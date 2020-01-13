5b486bff208d77d3683f5bbde3eb6e37157ad3aa
damianS7 <damian7mg@gmail.com>
2020-01-13 21:41:27 +0100

    Implementacion de validacion de Rooms

---
+++ app/Http/Requests/RoomRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RoomRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'room.name' => 'required|string|max:30',
+            'room.type' => 'required|string|max:30',
+            'room.floor' => 'required|string|max:30',
+        ];
+    }
+}
