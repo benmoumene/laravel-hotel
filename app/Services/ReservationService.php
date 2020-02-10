abffa6b6105ec1663e6cebf0fb81f52d7a781786
damianS7 <josepwnz@gmail.com>
2020-02-10 22:36:06 +0100

    Servicio para las reservas.

---
+++ app/Services/ReservationService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services;
+
+class ReservationService
+{
+    // create reservation
+    public function reservation(App\Reservation $reservation)
+    {
+        // Comprobaciones
+
+        // return true/false
+    }
+}
