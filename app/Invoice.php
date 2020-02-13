dc03c2163e67ef8813ad9676493078ac7f1cdf64
damianS7 <josepwnz@gmail.com>
2020-02-13 20:00:37 +0100

    Corregido eloquent

--- app/Invoice.php
+++ app/Invoice.php
@@ -16,16 +16,28 @@ class Invoice extends Model
     {
         return $this->billedServices->sum('service.cost');
     }
-
+    
+    // SQL Original
+    //SELECT tCustomers.id, tReservations.id, tInvoices.id
+    //FROM customers AS tCustomers
+    //INNER JOIN reservations tReservations ON tCustomers.id = tReservations.customer_id
+    //INNER JOIN invoices tInvoices ON tInvoices.reservation_id = tReservations.id
+    //WHERE tInvoices.reservation_id IN (58, 59)
+    // ---------
+    // SQL Eloquent
+    //SELECT tCustomers.id, tReservations.id
+    //FROM customers AS tCustomers
+    //INNER JOIN reservations tReservations ON tCustomers.id = tReservations.customer_id
+    //WHERE tReservations.id IN (58, 59)
     public function customer()
     {
         return $this->hasOneThrough(
             'App\Customer',
             'App\Reservation',
-            'id',
-            'id',
-            'id',
-            'customer_id'
+            'id', // ok
+            'id', // ok
+            'reservation_id',
+            'customer_id' // ok
         );
     }
 
