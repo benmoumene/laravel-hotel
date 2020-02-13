a1951ae866349d989adafa51871b5419c28e2159
damianS7 <josepwnz@gmail.com>
2020-02-13 23:07:48 +0100

    ReservationList implementa algunas funciones del antiguo Guest

--- resources/js/components/reservations/ReservationList.vue
+++ resources/js/components/reservations/ReservationList.vue
@@ -2,6 +2,28 @@
   <b-container fluid>
     <!-- User Interface controls -->
     <b-row>
+      <b-col lg="6" class="my-1">
+        <b-form-group
+          label="Filter"
+          label-cols-sm="3"
+          label-align-sm="right"
+          label-size="sm"
+          label-for="filterInput"
+          class="mb-0"
+        >
+          <b-input-group size="sm">
+            <b-form-input
+              v-model="filter"
+              type="search"
+              id="filterInput"
+              placeholder="Type to Search"
+            ></b-form-input>
+            <b-input-group-append>
+              <b-button :disabled="!filter" @click="filter = ''">Clear</b-button>
+            </b-input-group-append>
+          </b-input-group>
+        </b-form-group>
+      </b-col>
       <b-col sm="6" class="my-1">
         <b-form-group
           label="Status"
@@ -24,6 +46,20 @@
           class="my-0"
         ></b-pagination>
       </b-col>
+      <b-col sm="5" md="6" class="my-1">
+        <b-form-group
+          label="Filter On"
+          label-cols-sm="3"
+          label-align-sm="right"
+          label-size="sm"
+          description="Leave all unchecked to filter on all data"
+          class="mb-0"
+        >
+          <b-form-checkbox-group v-model="filterOn" class="mt-1">
+            <b-form-checkbox value="current_guests">Current Guests</b-form-checkbox>
+          </b-form-checkbox-group>
+        </b-form-group>
+      </b-col>
     </b-row>
 
     <!-- Main table element -->
@@ -42,25 +78,87 @@
       :sort-direction="sortDirection"
       @filtered="onFiltered"
     >
-      <template v-slot:cell(first_name)="row">{{ customer(row.item.customer_id).first_name }}</template>
+      <template v-slot:cell(first_name)="row">{{ row.item.customer.first_name }}</template>
       <template v-slot:cell(last_name)="row">{{ customer(row.item.customer_id).last_name }}</template>
       <template v-slot:cell(room_name)="row">
         <router-link
           :to="{path: '/room/' + row.item.room_id +'/edit'}"
         >{{ room(row.item.room_id).name }}</router-link>
       </template>
-      <template v-slot:cell(actions)="row">
-        <router-link :to="{path: row.item.id +'/show'}">
-          <b-button size="sm" variant="primary">Show</b-button>
-        </router-link>
-        <b-button size="sm" @click="cancelReservation(row.item)" variant="danger">Cancel</b-button>
+      <template v-slot:cell(actions)="{item}">
+        <b-button
+          v-b-tooltip.hover
+          title="Customer details"
+          size="sm"
+          variant="info"
+          @click="customerInfo(item.customer.id)"
+        >
+          <i class="fa fa-user"></i>
+        </b-button>
+        <b-button
+          v-b-tooltip.hover
+          title="Reservation details"
+          size="sm"
+          variant="info"
+          @click="reservationInfo(item.id)"
+        >
+          <i class="fa fa-calendar-alt"></i>
+        </b-button>
+        <b-button
+          v-b-tooltip.hover
+          title="Room details"
+          size="sm"
+          variant="info"
+          @click="roomInfo(item.room_id)"
+        >
+          <i class="fa fa-bed"></i>
+        </b-button>
+        <b-button
+          v-b-tooltip.hover
+          title="Services"
+          size="sm"
+          variant="success"
+          @click="billingInfo(item.id)"
+        >
+          <i class="nav-icon fa fa-coffee"></i>
+        </b-button>
+        <b-button
+          v-b-tooltip.hover
+          title="Invoice"
+          size="sm"
+          variant="success"
+          @click="invoiceInfo(item.id)"
+        >
+          <i class="nav-icon fa fa-file-invoice-dollar"></i>
+        </b-button>
       </template>
     </b-table>
+
+    <b-modal id="room-modal" size="xl" centered title="Room Info" hide-footer>
+      <room-info :roomId="roomId" :readonly="true"></room-info>
+    </b-modal>
+    <b-modal id="customer-modal" size="xl" centered title="Customer Info" hide-footer>
+      <customer-info :customerId="customerId" :readonly="true"></customer-info>
+    </b-modal>
+    <b-modal id="reservation-modal" size="xl" centered title="Reservation Info" hide-footer>
+      <reservation :reservationId="reservationId" :readonly="false" />
+    </b-modal>
+    <b-modal id="billing-modal" size="xl" centered title="Billing Info" hide-footer>
+      <billed-info :reservationId="reservationId" :readonly="true" />
+    </b-modal>
+    <b-modal id="invoice-modal" size="xl" centered title="Invoice Info" hide-footer>
+      <invoice-info :invoiceId="invoiceId" :readonly="true"></invoice-info>
+    </b-modal>
   </b-container>
 </template>
 <script>
 // Implementar filtro de fechas por defecto en RESERVATION LIST.
 import { mapState, mapGetters } from "vuex";
+import Room from "../rooms/Room";
+import Invoice from "../billing/Invoice";
+import BilledServices from "../billing/BilledServices";
+import ReservationEdit from "./ReservationEdit";
+import Customer from "../customers/Customer";
 export default {
   name: "ReservationList",
   data: function() {
@@ -103,10 +201,42 @@ export default {
       sortDirection: "asc",
       filter: null,
       onStatus: "active",
-      filterOn: []
+      filteredRows: null,
+      filterOn: [],
+      roomId: null,
+      guestId: null,
+      reservationId: null,
+      customerId: null,
+      invoiceId: null,
+      selectedReservation: null
     };
   },
   methods: {
+    reservationInfo(id) {
+      //this.selectedReservation = reservation;
+      this.reservationId = id;
+      this.$bvModal.show("reservation-modal");
+    },
+    roomInfo(id) {
+      this.roomId = id;
+      this.$bvModal.show("room-modal");
+    },
+    invoiceInfo(id) {
+      this.invoiceId = id;
+      this.$bvModal.show("invoice-modal");
+    },
+    billingInfo(reservation_id) {
+      this.reservationId = reservation_id;
+      this.$bvModal.show("billing-modal");
+    },
+    customerInfo(id) {
+      this.customerId = id;
+      this.$bvModal.show("customer-modal");
+    },
+    onFiltered(filteredItems) {
+      this.currentPage = 1;
+      this.filteredRows = filteredItems.length;
+    },
     customer(id) {
       return this.getCustomer(id);
     },
@@ -118,10 +248,6 @@ export default {
         vm: this,
         reservation
       });
-    },
-    onFiltered(filteredItems) {
-      // Trigger pagination to update the number of buttons/pages due to filtering
-      this.currentPage = 1;
     },
     makeToast(title, message, variant = "info") {
       this.$bvToast.toast(message, {
@@ -140,14 +266,25 @@ export default {
     }),
     ...mapGetters({
       getCustomer: "customer/getCustomer",
-      getRoom: "room/getRoom"
+      getRoom: "room/getRoom",
+      getGuest: "guest/getGuest",
+      getInvoice: "billing/getInvoiceFromReservation"
     }),
     filteredReservations() {
-      if (this.onStatus === "all") {
-        return this.reservations;
+      let newArray = [];
+      for (const reservation of this.reservations) {
+        reservation.customer = this.getCustomer(reservation.customer_id);
+        reservation.room = this.getRoom(reservation.room_id);
+        reservation.guest = this.getGuest(reservation.guest.id);
+        reservation.invoice = this.getInvoice(reservation.id);
+        newArray.push(reservation);
       }
 
-      return this.reservations.filter(
+      if (this.onStatus === "all") {
+        return newArray;
+      }
+
+      return newArray.filter(
         reservation => reservation.status === this.onStatus
       );
     },
@@ -160,8 +297,18 @@ export default {
         });
     },
     totalRows() {
+      if (this.filteredRows !== null) {
+        return this.filteredRows;
+      }
       return this.filteredReservations.length;
     }
+  },
+  components: {
+    "room-info": Room,
+    "customer-info": Customer,
+    "invoice-info": Invoice,
+    "billed-info": BilledServices,
+    reservation: ReservationEdit
   }
 };
 </script>
