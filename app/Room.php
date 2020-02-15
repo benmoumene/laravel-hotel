fc3c2fe7630869ecb03d8863b3d738039c8c84a6
damianS7 <josepwnz@gmail.com>
2020-02-15 22:21:13 +0100

    Limpieza del modelo Room

--- app/Room.php
+++ app/Room.php
@@ -7,25 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Room extends Model
 {
     public $timestamps = false;
-    protected $table = "rooms";
 
     protected $hidden = [
         'laravel_through_key'
     ];
 
-    // Guests hospeados actualmente en la habitacion
-    public function guest()
-    {
-        return $this->hasOne(
-            'App\Guest'
-        );
-    }
-
-    // Reservas para esta habitacion
-    public function reservations()
-    {
-        return $this->hasMany(
-            'App\Reservation'
-        );
-    }
+    protected $fillable = [
+        "name", "size", "type", "floor"
+    ];
 }
